7d89c26a67d5df422c0ebb263f86ee4742f159a9
linhlinh38 <03linhnguyen@gmail.com>
2024-10-08 00:35:20 +0700

    create index file and init postgre

---
+++ src/dataSource.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { config } from "./config/envConfig";
+import path from "path";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: config.DB_HOST,
+  port: config.DB_PORT,
+  username: config.DB_USER,
+  password: config.DB_PASS,
+  database: config.DB_NAME,
+
+  entities: [path.resolve(__dirname + "/entities/{*.js,*.ts}")],
+  migrations: [path.resolve(__dirname + "/migrations/{*.js,*.ts}")],
+});
---
+++ src/index.ts
@@ -0,0 +1,81 @@
+import express from "express";
+import { AppDataSource } from "./dataSource";
+import { config } from "./config/envConfig";
+import cors from "cors";
+import Logging from "./utils/Logging";
+import http from "http";
+import swaggerUi from "swagger-ui-express";
+import accountRouter from "./routes/account.route";
+import router from "./routes/index.route";
+
+const app = express();
+
+const StartServer = () => {
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  const options: cors.CorsOptions = {
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "X-Access-Token",
+    ],
+    credentials: true,
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    origin: "*",
+    preflightContinue: false,
+  };
+
+  app.use(cors(options));
+
+  // Log the request and response
+  app.use((req, res, next) => {
+    Logging.info(
+      `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`
+    );
+
+    res.on("finish", () => {
+      Logging.info(`STATUS: [${res.statusCode}]`);
+    });
+
+    next();
+  });
+
+  // Healthcheck
+  app.get("/ping", (req, res, next) =>
+    res.status(200).json({ hello: "world" })
+  );
+
+  //Routes
+  app.use("/app", router);
+  //app.use(errorHandler);
+
+  app.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(undefined, {
+      swaggerOptions: {
+        url: "/swagger.json",
+      },
+    })
+  );
+
+  http
+    .createServer(app)
+    .listen(config.PORT, () =>
+      Logging.info(`Server is running on port ${config.PORT}`)
+    );
+};
+AppDataSource.initialize()
+  .then(() => {
+    //   app.use(express.json());
+    //   app.get("/", (req, res) => {
+    //     return res.json("Established connection!");
+    //   });
+    //   return app.listen(config.PORT);
+    Logging.info("Established connection!");
+    StartServer();
+  })
+  .catch((error) => Logging.error(error));
