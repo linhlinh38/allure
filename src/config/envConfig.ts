f73fbccc1a131ba50b057933d708a328eb9cdf56
linhlinh38 <03linhnguyen@gmail.com>
2024-10-15 01:27:04 +0700

    setup jwt and bcrypt

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -1,12 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+const PORT = process.env.PORT as unknown as number | undefined;
+
+//Database
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT as unknown as number | undefined;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
-const PORT = process.env.PORT as unknown as number | undefined;
+
+//JWT Token
+const SECRET_KEY_FOR_ACCESS_TOKEN = process.env.SECRET_KEY_FOR_ACCESS_TOKEN;
+const SECRET_KEY_FOR_REFRESH_TOKEN = process.env.SECRET_KEY_FOR_REFRESH_TOKEN;
 
 export const config = {
   DB_HOST,
@@ -15,4 +21,6 @@ export const config = {
   DB_PASS,
   DB_NAME,
   PORT,
+  SECRET_KEY_FOR_ACCESS_TOKEN,
+  SECRET_KEY_FOR_REFRESH_TOKEN,
 };
---
+++ src/utils/jwt.ts
@@ -0,0 +1,17 @@
+import jwt, { VerifyOptions, SignOptions } from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { config } from "../config/envConfig";
+
+export function generateRefreshToken(userId: string) {
+  const payload = { userId: userId };
+  const refreshToken = jwt.sign(payload, config.SECRET_KEY_FOR_REFRESH_TOKEN, {
+    expiresIn: "7d",
+  });
+  return refreshToken;
+}
+
+export async function encryptedPassword(password: string) {
+  const salt = await bcrypt.genSalt(8);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+}
