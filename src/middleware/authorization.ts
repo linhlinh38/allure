31fd08ea7c44f903b3160ac4ffa041faccfd3efa
linhlinh38 <03linhnguyen@gmail.com>
2024-10-24 00:34:25 +0700

    add authorization

---
+++ src/middleware/authorization.ts
@@ -0,0 +1,26 @@
+import { Response, NextFunction } from "express";
+
+import { AuthRequest } from "./authentication";
+import { accountService } from "../services/account.service";
+
+export const Author = (roles: Array<string>) => {
+  return async (req: AuthRequest, res: Response, next: NextFunction) => {
+    const id = req.loginUser;
+
+    let account;
+    try {
+      account = await accountService.findBy(id, "id");
+    } catch (error) {
+      next(error);
+    }
+
+    if (account.length == 0)
+      return res.status(401).json({ message: "Invalid Account" });
+
+    if (roles.indexOf(account[0].role) > -1) next();
+    else
+      return res
+        .status(401)
+        .json({ message: `Unauthorization. This is page for ${roles}` });
+  };
+};
--- src/routes/account.route.ts
+++ src/routes/account.route.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { accountController } from "../controller/account.controller";
+import authentication from "../middleware/authentication";
+
 const accountRouter = express.Router();
 
+accountRouter.post("/", accountController.createAccount);
+
+accountRouter.use(authentication);
 accountRouter.get("/", accountController.getAllAccount);
 accountRouter.get("/profile/:option/:value", accountController.getAccountById);
-accountRouter.post("/", accountController.createAccount);
 accountRouter.put("/:id", accountController.updateAccount);
 accountRouter.delete("/:id", accountController.deleteAccount);
 export default accountRouter;
