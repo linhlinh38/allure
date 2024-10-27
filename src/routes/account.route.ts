f25ddd81d5fc20efab3d2dccd0c51b09508f6e0a
linhlinh38 <03linhnguyen@gmail.com>
2024-10-28 01:25:52 +0700

    add validate for account

--- src/routes/account.route.ts
+++ src/routes/account.route.ts
@@ -1,14 +1,28 @@
 import express from "express";
 import { accountController } from "../controllers/account.controller";
 import authentication from "../middleware/authentication";
+import validate from "../utils/validate";
+import {
+  AccountCreateSchema,
+  AccountUpdateSchema,
+} from "../dtos/request/account.request";
 
 const accountRouter = express.Router();
 
-accountRouter.post("/", accountController.createAccount);
+accountRouter.post(
+  "/",
+  validate(AccountCreateSchema),
+  accountController.createAccount
+);
 
 accountRouter.use(authentication);
 accountRouter.get("/", accountController.getAllAccount);
-accountRouter.get("/profile/:option/:value", accountController.getAccountById);
-accountRouter.put("/:id", accountController.updateAccount);
+accountRouter.get("/me", accountController.getMyProfile);
+accountRouter.get("/get/:option/:value", accountController.getAccountBy);
+accountRouter.put(
+  "/",
+  validate(AccountUpdateSchema),
+  accountController.updateAccount
+);
 accountRouter.delete("/:id", accountController.deleteAccount);
 export default accountRouter;
