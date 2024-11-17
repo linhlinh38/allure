3f98b97c8f153d0d61e5b3731ab12446783ba3c2
linhlinh38 <03linhnguyen@gmail.com>
2024-11-17 09:20:46 +0700

    implement crud product

--- src/entities/brand.entity.ts
+++ src/entities/brand.entity.ts
@@ -6,29 +6,29 @@ import {
   OneToMany,
   OneToOne,
   PrimaryGeneratedColumn,
-} from 'typeorm';
-import { BaseEntity } from './base.entity';
-import { StatusEnum } from '../utils/enum';
-import { Account } from './account.entity';
-import { Follow } from './follow.entity';
-import { Expose } from 'class-transformer';
+} from "typeorm";
+import { BaseEntity } from "./base.entity";
+import { StatusEnum } from "../utils/enum";
+import { Account } from "./account.entity";
+import { Follow } from "./follow.entity";
+import { Expose } from "class-transformer";
 
-@Entity('brands')
+@Entity("brands")
 export class Brand extends BaseEntity {
   @Expose()
-  @Column({ type: 'varchar', length: 100, unique: true })
+  @Column({ type: "varchar", length: 100, unique: true })
   name: string;
 
   @Expose()
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: "varchar", length: 255 })
   logo: string;
 
   @Expose()
-  @Column({ type: 'varchar', length: 255, nullable: false })
+  @Column({ type: "varchar", length: 255, nullable: false })
   document: string;
 
   @Expose()
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: "varchar", length: 255 })
   description: string;
 
   @ManyToMany(() => Account, (account) => account.brands)
@@ -38,22 +38,22 @@ export class Brand extends BaseEntity {
   follows: Follow[];
 
   @Expose()
-  @Column({ type: 'varchar', length: 255, nullable: false })
+  @Column({ type: "varchar", length: 255, nullable: false })
   email: string;
 
   @Expose()
-  @Column({ type: 'varchar', length: 15, nullable: true })
+  @Column({ type: "varchar", length: 15, nullable: true })
   phone: string;
 
   @Expose()
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: "varchar", length: 255 })
   address: string;
 
-  @Column({ type: 'float', default: 0 })
+  @Column({ type: "float", default: 0 })
   star: number;
 
   @Column({
-    type: 'enum',
+    type: "enum",
     enum: StatusEnum,
     default: StatusEnum.PENDING,
   })
