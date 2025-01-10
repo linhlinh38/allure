56a3f8cc366061e3f44ab70277a9d43f8bbca960
linhlinh38 <03linhnguyen@gmail.com>
2025-01-11 01:00:34 +0700

    update classification entity

--- src/dtos/request/productClassification.request.ts
+++ src/dtos/request/productClassification.request.ts
@@ -5,9 +5,12 @@ import { ProductImageCreateSchema } from "./productImage.request";
 export const ProductClassificationCreateSchema = z.object({
   body: z.object({
     title: z.string().min(1, "Title is required"),
-    sku: z.string().optional(),
+    sku: z.string(),
     price: z.number().int().nonnegative("Price must be non-negative"),
     quantity: z.number().int().nonnegative("Quantity must be non-negative"),
+    color: z.string().optional(),
+    size: z.string().optional(),
+    other: z.string().optional(),
     images: z.array(ProductImageCreateSchema.shape.body).optional(),
     type: z.nativeEnum(ClassificationTypeEnum),
     product: z.string().uuid().nullable().optional(),
